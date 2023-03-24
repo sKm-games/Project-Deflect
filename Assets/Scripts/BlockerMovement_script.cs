1a709da44079b1fc12aeca022ba7e943951e9cb0
ZenriS <skm2004@gmail.com>
2023-03-24 10:36:54 +0100

    Push of orginal Prototype

---
+++ Assets/Scripts/BlockerMovement_script.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockerMovement_script : MonoBehaviour
+{
+    [SerializeField] private bool allowMove;
+    public bool AllowMove
+    {
+        set
+        {
+            allowMove = value;
+        }
+    }
+
+    private GameObject blocker;
+
+    private void Start()
+    {
+        blocker = this.transform.GetChild(0).gameObject;
+        ToggleBlocker(false);
+    }
+
+    void Update()
+    {
+        if (allowMove)
+        {
+            RotateBlocker();
+        }
+    }
+
+    void RotateBlocker() //rotatet toward mouse
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 direction = new Vector2(mousePos.x - this.transform.position.x, mousePos.y - this.transform.position.y);
+
+        transform.up = direction;
+    }
+
+    public void ToggleBlocker(bool b)
+    {
+        allowMove = b;
+        blocker.SetActive(b);
+    }
+}
---
+++ Assets/Scripts/LifeTime_script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeTime_script : MonoBehaviour
+{
+    [SerializeField] float lifeTime;
+
+    private void Update()
+    {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }        
+    }
+}
